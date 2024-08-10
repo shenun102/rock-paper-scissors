acb32dc19bdf5fd9d97acbf640b4fca671ffd788
shenun102 <124623141+shenun102@users.noreply.github.com>
2024-08-10 01:15:38 +0100

    Add initiate game and play 5 rounds functionality
    Created a function that simulates a round and its
    outcome.
    
    Created a function that initiates the game for 5
    rounds. A score is tallied and the final victor
    is announced.

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-let humanScore = 0;
-let computerScore = 0;
-
+// Function to generate computer choice
 const getComputerChoice = function () {
   const num = Math.floor(Math.random() * 3) + 1;
   if (num === 1) {
-    return "Rock";
+    return "rock";
   } else if (num === 2) {
-    return "Paper";
+    return "paper";
   } else if (num === 3) {
-    return "Scissors";
+    return "scissors";
   }
 };
 
-console.log(getComputerChoice());
-
+// Function to prompt user choice
 const getHumanChoice = function () {
   const humanChoice = parseInt(
     prompt(`
-    Pick a number 1, 2 or 3
     1: Rock
     2: Paper
     3: Scissors`)
@@ -31,4 +27,63 @@ const getHumanChoice = function () {
   }
 };
 
-console.log(getHumanChoice());
+// Function to initiate game
+const playGame = function () {
+  // Declare score and final result variables
+  let humanScore = 0;
+  let computerScore = 0;
+  let finalScore;
+
+  // Function to play a round
+  const playRound = function (computerChoice, humanChoice) {
+    const human = humanChoice.toLowerCase();
+    console.log(`The computer chose (${computerChoice})`);
+    console.log(`You chose (${human})`);
+
+    // Outcomes of rock paper scissors
+    if (computerChoice === "rock" && human === "paper") {
+      humanScore++;
+      console.log(`You won! The score is ${humanScore}:${computerScore}`);
+    } else if (computerChoice === "rock" && human === "scissors") {
+      computerScore++;
+      console.log(`You lost! The score is ${humanScore}:${computerScore}`);
+    } else if (computerChoice === "paper" && human === "rock") {
+      computerScore++;
+      console.log(`You lost! The score is ${humanScore}:${computerScore}`);
+    } else if (computerChoice === "paper" && human === "scissors") {
+      humanScore++;
+      console.log(`You won! The score is ${humanScore}:${computerScore}`);
+    } else if (computerChoice === "scissors" && human === "rock") {
+      humanScore++;
+      console.log(`You won! The score is ${humanScore}:${computerScore}`);
+    } else if (computerChoice === "scissors" && human === "paper") {
+      computerScore++;
+      console.log(`You lost! The score is ${humanScore}:${computerScore}`);
+    } else {
+      console.log(`Draw!`);
+    }
+
+    // Determine the final victor by comparing scores
+    if (humanScore > computerScore) {
+      finalScore = `You, the player win!`;
+    } else if (humanScore < computerScore) {
+      finalScore = `You lost to the Computer!`;
+    } else {
+      finalScore = `It's a draw!`;
+    }
+  };
+
+  // Play 5 rounds
+  playRound(getComputerChoice(), getHumanChoice());
+  playRound(getComputerChoice(), getHumanChoice());
+  playRound(getComputerChoice(), getHumanChoice());
+  playRound(getComputerChoice(), getHumanChoice());
+  playRound(getComputerChoice(), getHumanChoice());
+
+  // Final victor announcement
+  console.log(
+    `The final score is ${humanScore}:${computerScore}, ${finalScore}`
+  );
+};
+
+playGame();
